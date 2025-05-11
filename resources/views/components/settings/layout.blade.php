7ce7def2a1d6e9110818c3c6ab3cff5c94622f17
Martin Striednig <m.striednig@gmail.com>
2025-05-11 21:17:11 +0200

    Add Verification Page to Settings

--- resources/views/components/settings/layout.blade.php
+++ resources/views/components/settings/layout.blade.php
@@ -2,15 +2,12 @@
     <div class="me-10 w-full pb-4 md:w-[220px]">
         <flux:navlist>
             <flux:navlist.item :href="route('settings.profile')" wire:navigate>{{ __('Profile') }}</flux:navlist.item>
-            <flux:navlist.item :href="route('settings.preferences')" wire:navigate>{{ __('Preferences') }}
-            </flux:navlist.item>
+            <flux:navlist.item :href="route('settings.preferences')" wire:navigate>{{ __('Preferences') }}</flux:navlist.item>
+            <flux:navlist.item :href="route('profile.verify')" wire:navigate>{{ __('Profile Verification') }}</flux:navlist.item>
             <flux:navlist.item :href="route('settings.password')" wire:navigate>{{ __('Password') }}</flux:navlist.item>
-            <flux:navlist.item :href="route('settings.appearance')" wire:navigate>{{ __('Appearance') }}
-            </flux:navlist.item>
-            <flux:navlist.item :href="route('settings.privacy-settings')" wire:navigate>{{ __('Privacy') }}
-            </flux:navlist.item>
-            <flux:navlist.item :href="route('settings.account-deletion')" wire:navigate>{{ __('Delete Account') }}
-            </flux:navlist.item>
+            <flux:navlist.item :href="route('settings.appearance')" wire:navigate>{{ __('Appearance') }}</flux:navlist.item>
+            <flux:navlist.item :href="route('settings.privacy-settings')" wire:navigate>{{ __('Privacy') }}</flux:navlist.item>
+            <flux:navlist.item :href="route('settings.account-deletion')" wire:navigate>{{ __('Delete Account') }}</flux:navlist.item>
         </flux:navlist>
     </div>
 
