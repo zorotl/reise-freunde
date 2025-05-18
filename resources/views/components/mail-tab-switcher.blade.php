68f319a93347bf675cf860f8fc58cae2bc75f3ea
Martin Striednig <m.striednig@gmail.com>
2025-05-18 13:41:55 +0200

    Message Archive/Delete: Add Nav for ArchivedBox

--- resources/views/components/mail-tab-switcher.blade.php
+++ resources/views/components/mail-tab-switcher.blade.php
@@ -1,23 +1,31 @@
 <div class="mb-6 flex flex-wrap gap-4 border-b border-gray-200 dark:border-neutral-600">
     <a href="{{ route('mail.inbox') }}" @class([ 'pb-2 font-medium transition' , request()->routeIs('mail.inbox')
-        ? 'text-indigo-600 border-b-2 border-indigo-600'
-        : 'text-gray-600 hover:text-indigo-500'
+        ? 'text-indigo-600 border-b-2 border-indigo-600 dark:text-indigo-400 dark:border-indigo-400'
+        : 'text-gray-600 hover:text-indigo-500 dark:text-gray-300 dark:hover:text-indigo-400'
         ])
         wire:navigate>
         {{ __('Inbox') }}
     </a>
 
     <a href="{{ route('mail.outbox') }}" @class([ 'pb-2 font-medium transition' , request()->routeIs('mail.outbox')
-        ? 'text-indigo-600 border-b-2 border-indigo-600'
-        : 'text-gray-600 hover:text-indigo-500'
+        ? 'text-indigo-600 border-b-2 border-indigo-600 dark:text-indigo-400 dark:border-indigo-400'
+        : 'text-gray-600 hover:text-indigo-500 dark:text-gray-300 dark:hover:text-indigo-400'
         ])
         wire:navigate>
         {{ __('Outbox') }}
     </a>
 
+    <a href="{{ route('mail.archive') }}" @class([ 'pb-2 font-medium transition' , request()->routeIs('mail.archive')
+        ? 'text-indigo-600 border-b-2 border-indigo-600 dark:text-indigo-400 dark:border-indigo-400'
+        : 'text-gray-600 hover:text-indigo-500 dark:text-gray-300 dark:hover:text-indigo-400'
+        ])
+        wire:navigate>
+        {{ __('Archive') }}
+    </a>
+
     <a href="{{ route('mail.compose') }}" @class([ 'pb-2 font-medium transition' , request()->routeIs('mail.compose')
-        ? 'text-indigo-600 border-b-2 border-indigo-600'
-        : 'text-gray-600 hover:text-indigo-500'
+        ? 'text-indigo-600 border-b-2 border-indigo-600 dark:text-indigo-400 dark:border-indigo-400'
+        : 'text-gray-600 hover:text-indigo-500 dark:text-gray-300 dark:hover:text-indigo-400'
         ])
         wire:navigate>
         {{ __('Compose') }}
