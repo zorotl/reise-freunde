c43dd21d01a1e473ae011d02c65a014f213dc838
Martin Striednig <m.striednig@gmail.com>
2025-06-04 20:39:48 +0200

    footer fix

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,10 +4,10 @@
             &copy; {{ date('Y') }} {{ config('app.name', 'Travel Together') }}. {{ __('All rights reserved.') }}
         </div>
         <nav class="space-x-4">
-            <a href="{{ route('imprint') }}" class="hover:underline">Imprint</a>
-            <a href="{{ route('privacy') }}" class="hover:underline">Privacy Policy</a>
-            <a href="{{ route('terms') }}" class="hover:underline">Terms &amp; Conditions</a>
-            <a href="{{ route('cookies') }}" class="hover:underline">Cookies</a>
+            <a href="{{ route('imprint') }}" class="hover:underline">{{__('Imprint')}}</a>
+            <a href="{{ route('privacy') }}" class="hover:underline">{{__('Privacy Policy')}}</a>
+            <a href="{{ route('terms') }}" class="hover:underline">{{__('Terms & Conditions')}}</a>
+            <a href="{{ route('cookies') }}" class="hover:underline">{{__('Cookies')}}</a>
         </nav>
     </div>
 </footer>
